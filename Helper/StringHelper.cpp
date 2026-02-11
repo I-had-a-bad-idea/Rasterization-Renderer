9688ffab53e91bfe7b93b723c582b04f32438573
I-had-a-bad-idea <182666156+I-had-a-bad-idea@users.noreply.github.com>
2026-02-11 16:00:32 +0100

    comments for helper/

--- Helper/StringHelper.cpp
+++ Helper/StringHelper.cpp
@@ -3,14 +3,14 @@
 #include <sstream>
 #include <vector>
 #include "StringHelper.h"
-
+// Read entire file into a string
 std::string StringHelper::readFileToString(const std::string& path){
     std::ifstream file(path);
     std::stringstream buffer;
     buffer << file.rdbuf();
     return buffer.str();
 }
-
+// Split string by delimiter
 std::vector<std::string> StringHelper::split(const std::string& s, char delimiter) {
     std::vector<std::string> tokens;
     std::string token;
